bfbb56eed82f3f04c3dfd754231eeeb51903afb1
GuitarmonYz <guitarmonyz@gmail.com>
2017-11-05 15:04:42 -0500

    finish header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -10,15 +10,25 @@
                     <span class="name">{{seller.name}}</span>
                 </div>
                 <div class="description">
-                    {{seller.description}}/{{seller.deliveryTime}}min delivered
+                    {{seller.description}}/{{seller.deliveryTime}} min delivered
                 </div>
                 <div v-if="seller.supports" class="support">
                     <span class="icon" :class="classMap[seller.supports[0].type]"></span>
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
+            <div v-if="seller.supports" class="support-count">
+                <span class="count">{{seller.supports.length}} more</span>
+                <i class="icon-keyboard_arrow_right"></i>
+            </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" width="100%" height="100%">
+        </div>
     </div>
 </template>
 
@@ -38,17 +48,21 @@
 <style lang="stylus" rel="stylesheet/stylus">
     @import "../../common/stylus/mixin"
     .header
+      position: relative
       color: #fff
-      background: #000
+      background: rgba(7, 17, 27, 0.5)
       .content-wrapper
+        position: relative
         padding: 24px 12px 18px 24px
+        font-size: 0
         .avatar
           display: inline-block
           vertical-align: top
+          img
+            border-radius: 2px
         .content
           display: inline-block
           margin-left: 16px
-          font-size: 14px
           .title
             margin: 2px 0 8px 0
             .brand
@@ -61,19 +75,20 @@
               background-repeat: no-repeat
             .name
               margin-left: 6px
-              font-szie: 16px
+              font-size: 16px
               line-height: 18px
               font-weight: bold
           .description
-            margin-top: 10px
+            margin-bottom: 10px
             line-height: 12px
             font-size: 12px
           .support
             .icon
               display: inline-block
+              vertical-align: top
               width: 12px
               height: 12px
-              margin-right: 4px
+              margin-right: 2px
               background-size: 12px 12px
               background-repeat: no-repeat
               &.decrease
@@ -86,5 +101,61 @@
                 bg-image('invoice_1')
               &.special
                 bg-image('special_1')
+            .text
+              line-height: 12px
+              font-size: 12px
 
+        .support-count
+          position: absolute
+          right: 12px
+          bottom: 14px
+          padding: 0 8px
+          height: 24px
+          line-height: 24px
+          border-radius: 14px
+          background: rgba(0, 0, 0, 0.2)
+          text-align: center 
+          .count
+            vertical-align: top
+            font-size: 10px
+          .icon-keyboard_arrow_right
+            margin-left: 2px
+            font-size: 10px  
+            line-height: 24px
+
+      .bulletin-wrapper
+        position: relative
+        height: 28px
+        line-height: 28px
+        padding: 0 22px 0 12px
+        white-space: nowrap
+        overflow: hidden
+        text-overflow: ellipsis
+        background: rgba(7, 17, 27, 0.2)
+        .bulletin-title
+          display: inline-block
+          vertical-align: top
+          margin-top: 8px
+          width: 22px
+          height: 12px
+          bg-image('bulletin')
+          background-size: 22px 12px
+          background-repeat: no-repeat
+        .bulletin-text
+          vertical-align: top
+          margin: 0 4px
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          position: absolute
+          font-size: 10px
+          right: 12px
+          top: 10px
+      .background
+        position: absolute
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+        z-index: -1
+        filter: blur(10px)
 </style>
